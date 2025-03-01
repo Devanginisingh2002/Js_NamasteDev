e319772b863d88d9dfce40acbf9ed9c268e8963f
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-03-01 23:31:30 +0530

    new

--- scr.js
+++ scr.js
@@ -55,8 +55,29 @@
 // const c = 909;
 
 //attach with window object
-let a = 10;
-console.log(a);
-var b = 100;
+// let a = 10;
+// console.log(a);
+// var b = 100;
 
 
+// Ep: 9
+// var a = 100;
+// {
+//     var a = 20;
+//     let b = 30;
+//     const c = 40;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+// console.log(a);
+
+// EP: 10
+function x(){
+    var a = 7;
+    function y(){
+        console.log(a);
+    } 
+    y();
+}
+x();
