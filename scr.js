4175fdf6b70139027ecb2fb0ce562c96c85a25da
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-03-03 21:19:49 +0530

    check

--- scr.js
+++ scr.js
@@ -61,16 +61,16 @@
 
 
 // Ep: 9
-// var a = 100;
-// {
-//     var a = 20;
-//     let b = 30;
-//     const c = 40;
-//     console.log(a);
-//     console.log(b);
-//     console.log(c);
-// }
-// console.log(a);
+var a = 100;
+{
+    var a = 20;
+    let b = 30;
+    const c = 40;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+console.log(a);
 
 // EP: 10
 function x(){
