cc483c2c83ed57fd8b679b2142189d350e2fe856
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-03-11 23:42:55 +0530

    file

--- scr.js
+++ scr.js
@@ -61,16 +61,16 @@
 
 
 // Ep: 9
-var a = 100;
-{
-    var a = 20;
-    let b = 30;
-    const c = 40;
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-console.log(a);
+// var a = 100;
+// {
+//     var a = 20;
+//     let b = 30;
+//     const c = 40;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+// console.log(a);
 
 // EP: 10
 function x(){
@@ -78,6 +78,36 @@ function x(){
     function y(){
         console.log(a);
     } 
-    y();
+    return y;
 }
-x();
+var z = x();
+console.log(z);
+z();
+
+//output of the code
+function x(){
+    var a = 7;
+
+    function y() {
+        console.log(a);
+    }
+    a=100;
+    return y;
+}
+var z = x();
+console.log(x);
+z();
+
+//2
+function z(){
+    var b = 900
+    function x(){
+        var a = 7;
+        function y(){
+            console.log(a, b)
+        }
+        y()
+    }
+    x()
+}
+z()
