646e2143bc038711166ce0ce8e5ebb8d3791865a
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-03-22 23:20:27 +0530

    file

---
+++ script.js
@@ -0,0 +1,17 @@
+// Episode:1 -> How JS is executed & Call Stack.
+"Everything in JS happens inside an Execution Context"
+/*
+
+|-----------------------------------------|
+|    MEMORY         |      CODE           |
+| (Variable Envt)   |(Thread of Execution)|
+|-----------------------------------------|
+|   Variable,       |                     |     
+|   function        |                     |
+|   stored as       |                     | 
+|   key, value      |                     | 
+|   pair.           |                     | 
+|                   |                     | 
+|___________________|_____________________|
+
+*/
