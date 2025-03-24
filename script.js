a13950af3eecdced71df61b38432a6308e7b2d3d
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-03-24 22:34:43 +0530

    file

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// Episode:1 -> How JS is executed & Call Stack.
-"Everything in JS happens inside an Execution Context"
+// Episode:1 -> How JS Works & Execution Context.
+"Everything in JS happens inside an Execution Context";
 /*
-
+o-> Execution Context: A big container, consist MEMORY & CODE.
 |-----------------------------------------|
 |    MEMORY         |      CODE           |
 | (Variable Envt)   |(Thread of Execution)|
 |-----------------------------------------|
-|   Variable,       |                     |     
+|   Variable,       |   Code Execution    |     
 |   function        |                     |
 |   stored as       |                     | 
 |   key, value      |                     | 
@@ -14,4 +14,46 @@
 |                   |                     | 
 |___________________|_____________________|
 
+
+o-> MEMORY: (Also k/w as Variable envt) -> All variable, fxn stored as key/value pair.
+o-> CODE: (Also k/w as Thread of Execution) -> Code run one at a time.
+
+o-> JS is a Synchronous(in-sequence) single-threaded language
+*/
+
+// Episode:2 -> How JS code is Executed? & Call Stack.
+"code";
+/* var n = 2;
+function square(num) {
+    var ans = num * num;
+    return ans;
+}
+
+var square2 = square(n);
+var square4 = square(4);
+
+o-> Execution Context for above code
+
+|-----------------------------------------|
+|    MEMORY         |      CODE           |
+| (Variable Envt)   |(Thread of Execution)|
+|-----------------------------------------|
+|   n: undefined,   |   Code Execution    |     
+| square: {...} fxn |                     |
+| square2: undefined|                     | 
+| square4: undefined|                     | 
+|                   |                     | 
+|                   |                     | 
+|___________________|_____________________|
+
+o-> 'return keyword': Done with the work of function, now give the control to the execution context, where function is invoke.
+
+o-> 'callstack': Managing Execution context in JS, call-stack maintains the order of execution of execution contexts.
+
+o-> 'callstack' also known as these following names
+    + Execution Context Stack
+    + Program Stack
+    + Control Stack
+    + Runtime Stack
+    + Machine Stack
 */
