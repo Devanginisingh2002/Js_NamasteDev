a352b8fca36f458e5169a79408eb53c5c77edb0b
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-04-12 00:37:32 +0530

    file

--- check.java
+++ check.java
@@ -1,15 +1,30 @@
-import java.util.*;
-public class check {
-    public static boolean sorted(int arr[]){
-        for(int i=0; i<arr.length-1; i++){
-            if(arr[i] > arr[i+1]){
-                return false;
-            }
+class Solution {
+
+    // Function to find maximum circular subarray sum.
+    public int circularSubarraySum(int[] arr) {
+        int totalSum = arr[0];
+        int maxSum = arr[0];
+        int currentMax = arr[0];
+        int minSum = arr[0];
+        int currentMin = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            // Kadane's for max subarray sum (non-circular)
+            currentMax = Math.max(arr[i], currentMax + arr[i]);
+            maxSum = Math.max(maxSum, currentMax);
+
+            // Kadane's for min subarray sum
+            currentMin = Math.min(arr[i], currentMin + arr[i]);
+            minSum = Math.min(minSum, currentMin);
+
+            // Total array sum
+            totalSum += arr[i];
         }
-        return true;
-    }
-    public static void main(String args[]){
-        int arr[] ={5, 6, 1, 2};
-        System.out.println(sorted(arr));
+
+        // If all elements are negative, maxSum will be the answer
+        if (maxSum < 0) return maxSum;
+
+        // Else, the answer is max of normal and circular case
+        return Math.max(maxSum, totalSum - minSum);
     }
 }
