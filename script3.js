259d8eec25e936cfda268878777659a19a7ca4e1
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-04-26 23:17:15 +0530

    file

--- script3.js
+++ script3.js
@@ -71,3 +71,51 @@ function attachEventListener() {
 }
 
 attachEventListener();
+
+
+class Solution {
+    // Function to merge the arrays.
+    public void mergeArrays(int a[], int b[]) {
+        int n = a.length;
+        int m = b.length;
+        int gap = nextGap(n + m);
+        
+        while (gap > 0) {
+            int i = 0;
+            int j = gap;
+            
+            while (j < (n + m)) {
+                // i-th element
+                int first = (i < n) ? a[i] : b[i - n];
+                // j-th element
+                int second = (j < n) ? a[j] : b[j - n];
+                
+                if (first > second) {
+                    // swap if out of order
+                    if (i < n && j < n) {
+                        int temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    } else if (i < n && j >= n) {
+                        int temp = a[i];
+                        a[i] = b[j - n];
+                        b[j - n] = temp;
+                    } else {
+                        int temp = b[i - n];
+                        b[i - n] = b[j - n];
+                        b[j - n] = temp;
+                    }
+                }
+                i++;
+                j++;
+            }
+            gap = nextGap(gap);
+        }
+    }
+    
+    // Helper function to calculate the next gap
+    private int nextGap(int gap) {
+        if (gap <= 1) return 0;
+        return (gap / 2) + (gap % 2);
+    }
+}
