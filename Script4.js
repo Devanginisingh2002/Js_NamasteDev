5b0ca25c40382833f4ff5889b17551737513466f
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-05-09 22:11:30 +0530

    JavaScript2

--- Script4.js
+++ Script4.js
@@ -240,3 +240,25 @@ class Solution {
         return false;
     }
 }
+class Solution {
+    // Function to search a given number in row-column sorted matrix.
+    public boolean searchMatrix(int[][] mat, int x) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int i = 0, j = m - 1; // start from top-right corner
+
+        while (i < n && j >= 0) {
+            if (mat[i][j] == x) {
+                return true;
+            } else if (mat[i][j] > x) {
+                j--; // move left
+            } else {
+                i++; // move down
+            }
+        }
+
+        return false;
+    }
+}
+
